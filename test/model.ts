9f498ebadba37fed3093b0c988d10429daa85872
jakubpavlicko22 <jakubpavlicko22@gmail.com>
2024-03-05 14:33:53 +0100

    Update tests

--- test/model.ts
+++ test/model.ts
@@ -1,12 +1,13 @@
-import {accessFilter, applicationCreatedBetween, applicationModel, jobModel} from "./test";
+import {accessFilter, applicationCreatedBetween, applicationModel, jobCreatedBetween, jobModel} from "./test";
 import assert from "node:assert";
+import {ObjectId} from "mongodb";
+import {LOG} from "../src/helpers";
 
 const accessFilterPipeline = [ { "$match": accessFilter } ];
-// const engagementsPipeline = [ { "$replaceWith": "$child" } ];
+const engagementsPipeline = [ {$unwind: '$engagements'}, {$replaceWith: '$engagements'} ];
 const applicationPipeline = [{$unwind: '$engagements'}, {$unwind: '$engagements.applications'}, {$replaceWith: '$engagements.applications'}];
 
 const betweenFilter = { between: { from: new Date('2024-01-01'), to: new Date('2024-12-01') } };
-// const arrayPropertyPipeline = [ { "$unwind": "$children" }, { "$replaceWith": "$children" } ];
 
 describe('AbstractModel - job', () =>
 {
@@ -24,13 +25,13 @@ describe('AbstractModel - job', () =>
     it('should create pipeline with custom filter', async () =>
     {
         const pipeline = await jobModel.pipeline({ customFilter: { jobCreatedBetween: { between: { from: new Date('2024-01-01'), to: new Date('2024-12-01') } } } });
-        assert.deepStrictEqual( pipeline, [ ...accessFilterPipeline, { $match: { created: { $gte: new Date('2024-01-01'), $lt: new Date('2024-12-01') } } }]);
+        assert.deepStrictEqual( pipeline, [ ...accessFilterPipeline, { $match: { 'events.created': { $gte: new Date('2024-01-01'), $lt: new Date('2024-12-01') } } }]);
     });
 
     it('should combine all options', async () =>
     {
         const pipeline = await jobModel.pipeline({ filter: { name: 'a' }, customFilter: { jobCreatedBetween: { between: { from: new Date('2024-01-01'), to: new Date('2024-12-01') } } } });
-        assert.deepStrictEqual( pipeline, [ ...accessFilterPipeline, { $match: { name: 'a' } }, { $match: { created: { $gte: new Date('2024-01-01'), $lt: new Date('2024-12-01') } } }]);
+        assert.deepStrictEqual( pipeline, [ ...accessFilterPipeline, { $match: { name: 'a' } }, { $match: { 'events.created': { $gte: new Date('2024-01-01'), $lt: new Date('2024-12-01') } } }]);
     });
 })
 
@@ -75,20 +76,29 @@ describe('AbstractPropertyModel - application', () =>
         ]);
     });
 
-    it('should combine application and job custom filters - pipeline + pipeline', async () =>
+    it('should combine application and job custom filters', async () =>
     {
-        const pipeline = await applicationModel.pipeline({ customFilter: { applicationCreatedBetween: betweenFilter, jobCreatedBetween: betweenFilter }, filter: { name: 'a' } });
+        const pipeline = await applicationModel.pipeline({ customFilter: { applicationCreatedBetween: betweenFilter, jobCreatedBetween: betweenFilter }, filter: { name: 'a', '$root.engagements.agencyID': new ObjectId('65e7053f3c67bebc2e959378'), status: 'dropout' } });
+        LOG(pipeline);
         assert.deepStrictEqual( pipeline, [
             applicationPipeline[0],
             applicationPipeline[1],
-            { "$match": { 'engagements.applications.name': 'a' } },
+            {
+                "$match": {
+                    'engagements.applications.status': 'dropout',
+                    'engagements.agencyID': new ObjectId('65e7053f3c67bebc2e959378'),
+                    'engagements.applications.name': 'a'
+                }
+            },
             applicationPipeline[2],
-            applicationCreatedBetween(betweenFilter),
-            { "$match": { created: { $gte: new Date('2024-01-01'), $lt: new Date('2024-12-01') } } },
+            ...jobCreatedBetween(betweenFilter),
+            ...applicationCreatedBetween(betweenFilter),
         ]);
     });
 
-    it('should combine application and job custom filters - pipeline + property');
-    it('should combine application and job custom filters - property + pipeline');
     it('should combine application and job custom filters - property + property');
+
+    it('should combine application and position custom filters - property + property');
+
+    it('should combine application, engagement and position custom filters - pipeline + pipeline + pipeline');
 })
