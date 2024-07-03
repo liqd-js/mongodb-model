d2624686bfe23effdb3ad1d93df655ae1d753c78
Tomáš Korenko <radixxko@gmail.com>
2024-07-03 13:26:00 +0200

    Update property-model.ts

--- src/property-model.ts
+++ src/property-model.ts
@@ -193,7 +193,7 @@ export abstract class AbstractPropertyModel<
         return _id;
     }*/
 
-    public async update( id: DTO['id'], update: Partial<DBE> | UpdateFilter<DBE> ): Promise<ModelUpdateResponse>
+    public async update( id: DTO['id'], update: Partial<DBE> | UpdateFilter<DBE> ): Promise<ModelUpdateResponse<DBE>>
     {
         let path = this.paths.map( p => p.path ).join('.') + '.id';
         let operations: Partial<RootDBE> | UpdateFilter<RootDBE> = {};
