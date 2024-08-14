13bbdb965905b92a3f1bfdd0794f1a21671722da
jakubpavlicko22 <jakubpavlicko22@gmail.com>
2024-08-14 15:52:42 +0200

    Fix

--- test/helpers/utils.ts
+++ test/helpers/utils.ts
@@ -1,41 +1,40 @@
 import assert from "node:assert";
-import {deleteNullishProperties} from "../../src";
-import { describe, it } from 'node:test';
+import { deleteNullishProperties } from "../../src";
 
-describe('deleteNullishProperties', () => {
-    it('should delete null and undefined properties from an object', () => {
+describe('const res = deleteNullishProperties', () => {
+    it('should delete null andres properties from an object', () => {
         const obj = { a: null, b: undefined, c: 1 };
-        deleteNullishProperties(obj);
-        assert.deepStrictEqual(obj, { c: 1 });
+        const res = deleteNullishProperties(obj);
+        assert.deepStrictEqual(res, { c: 1 });
     });
 
     it('should delete null and undefined elements from an array', () => {
         const arr = [null, undefined, 1];
-        deleteNullishProperties(arr);
-        assert.deepStrictEqual(arr, [1]);
+        const res = deleteNullishProperties(arr);
+        assert.deepStrictEqual(res, [1]);
     });
 
     it('should delete null and undefined properties from nested objects', () => {
         const obj = { a: { b: null, c: undefined, d: 1 }, e: 2 };
-        deleteNullishProperties(obj);
-        assert.deepStrictEqual(obj, { a: { d: 1 }, e: 2 });
+        const res = deleteNullishProperties(obj);
+        assert.deepStrictEqual(res, { a: { d: 1 }, e: 2 });
     });
 
     it('should delete null and undefined elements from nested arrays', () => {
         const arr = [[null, undefined, 1], 2];
-        deleteNullishProperties(arr);
-        assert.deepStrictEqual(arr, [[1], 2]);
+        const res = deleteNullishProperties(arr);
+        assert.deepStrictEqual(res, [[1], 2]);
     });
 
     it('should handle empty objects', () => {
         const obj = {};
-        deleteNullishProperties(obj);
-        assert.deepStrictEqual(obj, {});
+        const res = deleteNullishProperties(obj);
+        assert.deepStrictEqual(res, {});
     });
 
     it('should handle empty arrays', () => {
         const arr: any = [];
-        deleteNullishProperties(arr);
-        assert.deepStrictEqual(arr, []);
+        const res = deleteNullishProperties(arr);
+        assert.deepStrictEqual(res, []);
     });
 });
